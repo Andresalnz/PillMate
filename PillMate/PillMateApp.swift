a4a09e816bb44abbd17f938bfd85a9a85c84a1f3
Andresalnz <andresaleunz@gmail.com>
2025-02-04 17:50:36 +0100

    Initial Commit

---
+++ PillMate/PillMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillMateApp.swift
+//  PillMate
+//
+//  Created by Andres Aleu on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PillMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
