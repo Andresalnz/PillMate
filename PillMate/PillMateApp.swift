3c386f3d993213bd8c1db5ceaa33172aa00d566d
Andresalnz <andresaleunz@gmail.com>
2025-03-14 18:08:59 +0100

    Edit provisional App

--- PillMate/PillMateApp.swift
+++ PillMate/PillMateApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PillMateApp: App {
     var body: some Scene {
         WindowGroup {
-            ReminderCalendarView(viewModel: ReminderCalendarVM())
+            ReminderCalendarView(viewModel: ReminderCalendarVM(currentDate: Date(), month: "", numberMonth: 0, days: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], numberWeekDay: 5))
                 .background(Color(red: 217/255, green: 220/255, blue: 214/255))
         }
     }
