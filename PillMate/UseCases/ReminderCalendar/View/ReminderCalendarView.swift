56249af4c5c26bf2dcb5034721d758d7bde96c8e
Andresalnz <andresaleunz@gmail.com>
2025-03-14 18:05:10 +0100

    Add evironmentObject

--- PillMate/UseCases/ReminderCalendar/View/ReminderCalendarView.swift
+++ PillMate/UseCases/ReminderCalendar/View/ReminderCalendarView.swift
@@ -18,6 +18,7 @@ struct ReminderCalendarView: View {
                 MonthCalendarView(nameMonth: viewModel.month, daysOfWeeks: viewModel.daysOfWeek , emptyDaysBeginMonth: viewModel.numberWeekDay, daysInMonth: viewModel.days)
                     .padding(.horizontal)
                     .tag(offset)
+                    .environmentObject(viewModel)
             }
         }
         .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
@@ -33,5 +34,6 @@ struct ReminderCalendarView: View {
 
 #Preview {
     ReminderCalendarView(viewModel: .preview)
+        .environmentObject(ReminderCalendarVM(currentDate: Date(), month: "", numberMonth: 0, days: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], numberWeekDay: 5))
         .background(Color(red: 217/255, green: 220/255, blue: 214/255))
 }
