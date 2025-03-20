a31a2924dca69d30612a1d606844c9e1881080b4
Andresalnz <andresaleunz@gmail.com>
2025-03-20 17:54:45 +0100

    Add Floating Button

---
+++ PillMate/UseCases/Components/Buttons/FloatingActionButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  FloatingActionButton.swift
+//  PillMate
+//
+//  Created by Andres Aleu on 20/3/25.
+//
+
+import SwiftUI
+
+struct FloatingActionButtonView: View {
+    
+    
+    var imageName: String
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: {
+            action()
+        }, label: {
+            Image(systemName: imageName)
+                .font(.title)
+                .foregroundColor(.white)
+                .padding()
+                .background(.blue)
+                .clipShape(Circle())
+                .shadow(radius: 5)
+            
+        })
+    }
+}
+
+#Preview {
+    FloatingActionButtonView(imageName: "plus", action: { print("He pulsado el botón") })
+    //ButtonFilterView(title: "Botón", action: { print("pulsado") })
+}
