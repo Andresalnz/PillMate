24b5f204ae5253a7b22640d74d3d35e406d3103d
Andresalnz <andresaleunz@gmail.com>
2025-06-10 19:33:44 +0200

    Add configuration preview

--- PillMate/PillMateApp.swift
+++ PillMate/PillMateApp.swift
@@ -12,8 +12,9 @@ import SwiftData
 struct PillMateApp: App {
     
     let container: ModelContainer = {
-        let schema = Schema([InformationMedication.self, ScheduledDose.self, ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        let isRunningForPreviews = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+        let schema = Schema([InformationMedication.self, ScheduledDose.self])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: isRunningForPreviews)
         do {
             
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
