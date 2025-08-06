532761a76f27de0f64da4c42b45817393bf5f8a6
Andresalnz <andresaleunz@gmail.com>
2025-08-06 12:40:00 +0200

    Add constants and extensions

---
+++ PillMate/Utils/Constants/Constants.swift
@@ -0,0 +1,40 @@
+//
+//  Constants.swift
+//  PillMate
+//
+//  Created by Andres Aleu on 5/8/25.
+//
+
+import Foundation
+import SwiftUI
+
+//MARK: - Colors
+struct Colors {
+    static let primary: String = "#2A9D8F"
+    static let secondary: String = "#E76F51"
+    static let background: String = "#F4F4F4"
+}
+
+//MARK: - Strings
+struct Strings {
+    static let infoMedication: String = "Información de la medicación"
+    static let schedule: String = "Frecuencia y horarios"
+    static let instructionsOfDose: String = "Instrucciones de toma"
+    static let durationTreatment: String = "Duración del tratamiento"
+    static let presentation: String = "Presentación"
+    static let nameMedication: String = "Nombre del medicamento"
+    static let frequency: String = "Frecuencia"
+    static let dosesPerDay: String = "Tomas al día"
+    static let dosesThoseDays: String = "Tomas esos días"
+    static let pickerHourFirsDose: String = "Hora de la primera dosis"
+    static let momentofDose: String = "Momentos de toma"
+    static let instructionsAditionals: String = "Instrucciones adicionales"
+    static let startTreatment: String = "Fecha de inicio del tratamiento"
+    static let duration: String = "Duración"
+    static let finishTreatment: String = "Fecha de finalización del tratamiento"
+    static let selectDays: String = "Seleccionar días"
+    static let pleaseSelectDay: String = "Por favor, selecciona un día"
+    static let finishTreatmentNumberOfDays: String = "Finalizará el: "
+    static let noReminder: String = "Sin recordatorio para el día de hoy"
+}
+
