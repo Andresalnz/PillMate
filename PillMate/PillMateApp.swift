e94edbf62920d96d49295e337663e271feebe63d
Andresalnz <andresaleunz@gmail.com>
2025-08-27 13:00:33 +0200

    Edit View and add container

--- PillMate/PillMateApp.swift
+++ PillMate/PillMateApp.swift
@@ -14,7 +14,6 @@ struct PillMateApp: App {
     @StateObject var lnManager: LocalNotificationManager = LocalNotificationManager()
     
     let container: ModelContainer = {
-       // let isRunningForPreviews = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
         let schema = Schema([InformationMedication.self, ScheduledDose.self])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
         do {
@@ -28,11 +27,9 @@ struct PillMateApp: App {
     }()
     
     var body: some Scene {
-        let md: MedicationModel = MedicationModel(id: UUID() , name: "", presentation: .pills, dose: "", frequency: .daily, timePerDay: 1, everyXDays: 1, days: [], firstDoseTime: .now, momentDose: .afterMeal, customInstructions: "", treatmentStartDate: .now, treatmentEndDate: .now, treatmentDuration: .untilSpecificDate, numbersOfDays: 7, treatmentEndforNumberOfDays: Date(), notes: "")
-       
         WindowGroup {
-           // ReminderCalendarView(viewModel: ReminderCalendarVM(month: "", numberMonth: 1, days: [1], numberWeekDay: 0) ).modelContainer(container)
-            HomeView(md: md, openSheet: false, lnManager: lnManager).modelContainer(container)
+            TabBarView()
         }
+        .modelContainer(container)
     }
 }
