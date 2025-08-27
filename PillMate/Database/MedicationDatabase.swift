75f80dbeb000fe5bbbdb7e72f8b9b72e7350449c
Andresalnz <andresaleunz@gmail.com>
2025-08-27 12:59:18 +0200

    Add function update and remove async

--- PillMate/Database/MedicationDatabase.swift
+++ PillMate/Database/MedicationDatabase.swift
@@ -9,7 +9,8 @@ import Foundation
 import SwiftData
 
 protocol DatabaseProtocol {
-   @MainActor func saveMediaction(_ medication: InformationMedication, scheduleDose: [ScheduledDose]) async throws
+   @MainActor func saveMediaction(_ medication: InformationMedication, scheduleDose: [ScheduledDose])  throws
+   @MainActor func update(scheduleDose: ScheduledDose, medication: InformationMedication)  throws 
     var context: ModelContext? { get }
 }
 
@@ -17,11 +18,21 @@ struct MedicationDatabase: DatabaseProtocol {
     var context: ModelContext?
     
     @MainActor
-    func saveMediaction(_ medication: InformationMedication, scheduleDose: [ScheduledDose]) async throws {
+    func saveMediaction(_ medication: InformationMedication, scheduleDose: [ScheduledDose])  throws {
         context?.insert(medication)
         for i in scheduleDose {
             context?.insert(i)
         }
-       try context?.save()
+        try context?.save()
     }
+    
+    @MainActor
+    func update(scheduleDose: ScheduledDose, medication: InformationMedication)  throws {
+        
+        context?.insert(medication)
+        context?.insert(scheduleDose)
+        
+        try context?.save()
+    }
+    
 }
