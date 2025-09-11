e284414a6a8f3e606f1ca92d8e3ba2d7dfb54909
Andresalnz <andresaleunz@gmail.com>
2025-09-11 18:36:32 +0200

    Add List and Query swiftdata

--- PillMate/UseCases/Calendar/View/Components/MonthCalendarView.swift
+++ PillMate/UseCases/Calendar/View/Components/MonthCalendarView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MonthCalendarView: View {
     
@@ -17,6 +18,21 @@ struct MonthCalendarView: View {
     let calendarGrid: [GridItem] = Array(repeating: GridItem(.flexible()), count: 7)
     
     @ObservedObject var viewModel: ReminderCalendarVM
+    @Query private var dose: [ScheduledDose]
+    
+    init(nameMonth: String, daysOfWeeks: [String], emptyDaysBeginMonth: Int, daysInMonth: [Int], viewModel: ReminderCalendarVM) {
+        self.nameMonth = nameMonth
+        self.daysOfWeeks = daysOfWeeks
+        self.emptyDaysBeginMonth = emptyDaysBeginMonth
+        self.daysInMonth = daysInMonth
+        self.viewModel = viewModel
+        let calendar = Calendar.current
+        let startOfDay = calendar.startOfDay(for: viewModel.selectedDay)
+        let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)
+        let predicate = #Predicate<ScheduledDose> { item in
+            item.scheduledTime >= startOfDay && item.scheduledTime < endOfDay!
+        }
+        _dose = Query(filter: predicate, sort: \ScheduledDose.scheduledTime)    }
     
     var body: some View {
         VStack {
@@ -42,13 +58,34 @@ struct MonthCalendarView: View {
                     
                 }
             }
-            if let selectedDay = viewModel.selectedDay {
-                ReminderCellView(day: selectedDay)
+            if dose.count == 0 {
+                VStack {
+                    Text("No hay recordatorios para el día de hoy.")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
             }
+            
+            List {
+                ForEach(dose, id:\.id) { dose in
+                    Section("\(dose.scheduledTime, style: .time)") {
+                        ReminderCellView(hourMedication: dose.scheduledTime, nameMedication: dose.medication.medicationName, doseMedication: dose.medication.medicationDose, presentationMedication: dose.medication.medicationPresentation, momentDoseMedication: dose.medication.medicationMomentDose, customInstructionsMedication: dose.medication.medicationCustomInstructions, statusDose: dose.status)
+                            .listRowSeparator(.hidden)
+                            .listRowBackground(Color.clear)
+                    }
+                    
+                }
+            }
+            .scrollContentBackground(.hidden)
+            .listStyle(.plain)
         }
     }
 }
 
 #Preview {
-    MonthCalendarView(nameMonth: "March", daysOfWeeks: ["L", "M", "X", "J", "V", "S", "D"], emptyDaysBeginMonth: 5, daysInMonth: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], viewModel: ReminderCalendarVM(month: "", numberMonth: 1, days: [], numberWeekDay: 1))
+    let preview = Preview()
+    preview.addExamples(ScheduledDose.sampleItems)
+    
+    return MonthCalendarView(nameMonth: "March", daysOfWeeks: ["L", "M", "X", "J", "V", "S", "D"], emptyDaysBeginMonth: 5, daysInMonth: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], viewModel: ReminderCalendarVM(month: "", numberMonth: 1, days: [], numberWeekDay: 1))
+        .modelContainer(preview.container)
 }
